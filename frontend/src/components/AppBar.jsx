a7f359d2487429794de6c65ec22838840cab77c4
Arnav-Nehra <arnavnehra1@gmail.com>
2024-09-21 22:36:32 +0530

    AppBar Created

---
+++ frontend/src/components/AppBar.jsx
@@ -0,0 +1,35 @@
+export function AppBar({firstName}){
+    return<div className=""> 
+        <div className="flex justify-between ">
+        <div className="flex justify-start px-4 pt-4">
+        <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
+        <path fill-rule="evenodd" 
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM9 7.5A.75.75 0 0 0 9 9h1.5c.98 0 1.813.626 2.122 1.5H9A.75.75 0 0 0 9 12h3.622a2.251 2.251 0 0 1-2.122 1.5H9a.75.75 0 0 0-.53 1.28l3 3a.75.75 0 1 0 1.06-1.06L10.8 14.988A3.752 3.752 0 0 0 14.175 12H15a.75.75 0 0 0 0-1.5h-.825A3.733 3.733 0 0 0 13.5 9H15a.75.75 0 0 0 0-1.5H9Z" 
+        clip-rule="evenodd" />
+        </svg>
+        </div>
+        <div className="font-bold text-2xl px-2">Phone Se</div>
+        </div>
+        <div className="flex justify-end pr-2 pt-4" >
+        <div className="text-xl">Welcome Back, {firstName}</div>
+        <div className="px-2 pr-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+    </div>
+    </div>
+    </div>
+
+
+
+    <div className="px-4 pt-4">
+    <div className="border-solid rounded-md ">
+    <div className="flex justify-center pt-20 text-xl font-mono ">
+    Your one stop point for easy payments
+    </div>
+    <div className="flex justify-center pt-4 text-xl font-mono ">Let's begin..</div></div>
+
+    </div>
+    
+    </div>
+}
